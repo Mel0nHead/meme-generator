12442e8bf632623354d8fca78c268af5b62804bc
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2021-08-13 20:03:10 +0100

    start working on add text functionality

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const uploadImage = document.querySelector("#upload-image");
 const fileInput = document.querySelector("#file-upload");
 const fileName = document.querySelector("#file-name");
 const canvas = document.getElementById("canvas");
+const editMemeControls = document.getElementById("edit-meme");
+const addTextInput = editMemeControls.querySelector("input");
+const addTextButton = editMemeControls.querySelector("button");
+
+// TODO:
+// need to work out how to make text draggable: http://jsfiddle.net/m1erickson/9xAGa/
+// should we use img or canvas? just thinking about exporting
+// could use ImgFlip API
 
 canvas.style.width = "100%";
 canvas.width = canvas.offsetWidth;
@@ -34,6 +42,7 @@ function handleFileInputChange() {
     img.onload = function () {
       scaleImageAndCanvas(img);
       context.drawImage(this, 0, 0, canvas.width, canvas.height);
+      editMemeControls.classList.add("visible");
       // For optimal performance and memory usage
       URL.revokeObjectURL(this.src);
     };
@@ -41,6 +50,11 @@ function handleFileInputChange() {
     fileName.textContent = file.name;
   }
 }
+
+addTextButton.addEventListener("click", function () {
+  const text = addTextInput.value;
+  addTextInput.value = "";
+});
 
 uploadImage.addEventListener("click", handleUploadImage);
 fileInput.addEventListener("change", handleFileInputChange);
