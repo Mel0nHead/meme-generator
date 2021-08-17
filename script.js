5e5f48261911e571e2c4a70dc5fb82bc0fee83bc
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2021-08-17 10:00:24 +0100

    work on text drag feature

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ const addTextButton = editMemeControls.querySelector("button");
 // need to work out how to make text draggable: http://jsfiddle.net/m1erickson/9xAGa/
 // should we use img or canvas? just thinking about exporting
 // could use ImgFlip API
+
 const customTexts = [];
+// Used to track the beginning position of the mouse when the user starts dragging text
+let mouseStartX, mouseStartY;
+let selectedTextIndex = -1;
 
 canvas.style.width = "100%";
 canvas.width = canvas.offsetWidth;
@@ -44,7 +48,11 @@ function drawImageToCanvas(src, textArray) {
     context.drawImage(this, 0, 0, canvas.width, canvas.height);
     editMemeControls.classList.add("visible");
     if (textArray && Array.isArray(textArray)) {
-      textArray.forEach(({ text, x, y }) => context.fillText(text, x, y));
+      context.font = "30px verdana";
+
+      textArray.forEach(({ text, x, y }) => {
+        context.fillText(text, x, y);
+      });
     }
     // For optimal performance and memory usage
     URL.revokeObjectURL(this.src);
@@ -66,24 +74,59 @@ function redrawToCanvas() {
   drawImageToCanvas(imgSrc, customTexts);
 }
 
+function isMouseWithinTextCoordinates(mouseX, mouseY, text) {
+  const textWidth = context.measureText(text.text).width;
+
+  return (
+    mouseX >= text.x &&
+    mouseX <= text.x + textWidth &&
+    mouseY <= text.y &&
+    mouseY >= text.y - text.height
+  );
+}
+
 function handleAddText() {
   const text = addTextInput.value;
   addTextInput.value = "";
+  const y = customTexts.length * 50 + 50;
 
   const textObj = {
     text,
     x: 20,
-    y: 20,
+    y,
   };
-  // TODO: all text seems to be really small - probably need to move this logic to `drawImageToCanvas`
-  context.font = "30px verdana";
-  textObj.width = context.measureText(textObj.text).width;
-  text.height = 16;
+
+  // TODO: fix height calculation as it seems a bit inaccurate
+  textObj.height = 30;
 
   customTexts.push(textObj);
   redrawToCanvas();
 }
 
+function handleCanvasMouseUp(e) {
+  e.preventDefault();
+  selectedTextIndex = -1;
+}
+
+function handleCanvasMouseOut(e) {
+  e.preventDefault();
+  selectedTextIndex = -1;
+}
+
+function handleCanvasMouseDown(e) {
+  mouseStartX = e.clientX - canvas.offsetLeft;
+  mouseStartY = e.clientY - canvas.offsetTop;
+
+  customTexts.forEach((text) => {
+    if (isMouseWithinTextCoordinates(mouseStartX, mouseStartY, text)) {
+      console.log(text);
+    }
+  });
+}
+
+canvas.addEventListener("mouseup", handleCanvasMouseUp);
+canvas.addEventListener("mouseout", handleCanvasMouseOut);
+canvas.addEventListener("mousedown", handleCanvasMouseDown);
 addTextButton.addEventListener("click", handleAddText);
 uploadImage.addEventListener("click", handleUploadImage);
 fileInput.addEventListener("change", handleFileInputChange);
